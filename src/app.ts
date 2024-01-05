5e7724c93ba413c35bd8a9ef1796f68b3ce89493
ebenezermensah <ebenezer.mensah@amalitech.org>
2024-01-05 13:55:59 +0000

    setup update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Express, Request, Response, Application } from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 
-app.use(morgan("combined"));
+app.use(morgan("dev"));
+// app.use(morgan("combined"));
+app.use(helmet());
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
